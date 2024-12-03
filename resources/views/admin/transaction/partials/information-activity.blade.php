86b7143693a7692ae1834ba79bc06f30d5c62cd5
keeptrain <19211020@bsi.ac.id>
2024-12-03 21:02:34 +0700

    feat(transaction): change ui checkbox for information to checklist

--- resources/views/admin/transaction/partials/information-activity.blade.php
+++ resources/views/admin/transaction/partials/information-activity.blade.php
@@ -6,11 +6,11 @@
     <!-- Quill Editor Container -->
     <div id="quill-editor" style="height: 150px;"></div>
 
-        <!-- Hidden Textarea untuk menyimpan konten Quill -->
+    <!-- Hidden Textarea untuk menyimpan konten Quill -->
     <textarea id="quill-editor-area" name="activity" rows="5" class="hidden">{{ isset($selectedProgram) ? $selectedProgram->activity : old('activity') }}</textarea>
-    
-   
-  <!-- Create toolbar container -->
+
+
+    <!-- Create toolbar container -->
     <!--div id="toolbar" class="dark:bg-zinc-600">
    
     <select class="ql-size dark:text-black dark:bg-zinc-600">
@@ -26,7 +26,7 @@
     <button class="ql-script" value="super"></button>
   </div-->
 
-    
+
 </div>
 
 @section('formbody2-section2')
@@ -36,94 +36,90 @@
             <x-admin.input-label for="tujuan-program" class="mb-2">
                 Tujuan
             </x-admin.input-label>
-            
-            <x-admin.text-input type="text" id="tujuan-program" name="objective"
-                class=" w-full p-2.5"
+
+            <x-admin.text-input type="text" id="tujuan-program" name="objective" class=" w-full p-2.5"
                 value="{{ isset($selectedProgram) ? $selectedProgram->objective : old('objective') }}"
-                placeholder="Masukkan tujuan"/>
+                placeholder="Masukkan tujuan" />
         </div>
-    
+
         <!-- Spacer -->
         <div class="flex items-center justify-center pb-2.5 w-6 h-6">
-    
+
         </div>
-    
+
         <!-- Output Program -->
         <div>
             <x-admin.input-label for="output-program" class="mb-2">
                 Output
             </x-admin.input-label>
-            <x-admin.text-input type="text" id="output-program" name="output"
-                class=" w-full p-2.5"
+            <x-admin.text-input type="text" id="output-program" name="output" class=" w-full p-2.5"
                 value="{{ isset($selectedProgram) ? $selectedProgram->output : old('output') }}"
-                placeholder="Masukkan output "/>
+                placeholder="Masukkan output " />
         </div>
-    
+
         <!-- Output Program -->
         <div>
             <x-admin.input-label for="target-program" class="mb-2">
                 Target
             </x-admin.input-label>
-            <x-admin.text-input  type="text" id="output-program" name="target"
-                class=" w-full p-2.5"
+            <x-admin.text-input type="text" id="output-program" name="target" class=" w-full p-2.5"
                 value="{{ isset($selectedProgram) ? $selectedProgram->target : old('target') }}"
-                placeholder="Masukkan output "/>
+                placeholder="Masukkan output " />
         </div>
-    
-    
-    
+
+
+
         <!-- Spacer -->
         <div class="flex items-center justify-center pb-2.5 w-6 h-6">
-    
+
         </div>
-    
+
         <!-- Volume Program -->
         <div>
             <x-admin.input-label for="volume-program" class="mb-2">
                 Volume
             </x-admin.input-label>
-            <x-admin.text-input type="number" id="input-volume" name="volume"
-                class=" w-full p-2.5"
+            <x-admin.text-input type="number" id="input-volume" name="volume" class=" w-full p-2.5"
                 value="{{ isset($selectedProgram) ? $selectedProgram->volume : old('volume') }}"
-                placeholder="Berapa kali diadakan dalam 1 tahun"/>
+                placeholder="Berapa kali diadakan dalam 1 tahun" />
         </div>
-    
-       <!-- Keterangan -->
+
+        <!-- Keterangan -->
         <div>
             <x-admin.input-label for="information-program" class="mb-2">
                 Keterangan
             </x-admin.input-label>
-            <div class="w-full max-w-2xl flex flex-col rounded-lg bg-white dark:bg-zinc-900 shadow p-3">
-                <nav class="flex flex-row justify-between">
-                    <div class="flex items-center flex-1 justify-center">
-                        <input type="radio" name="information" value="belum_terlaksana"
-                            class="h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-blue-800 checked:border-slate-800"
-                            {{ (old('information', isset($selectedProgram) ? $selectedProgram->information : '') == 'belum_terlaksana') ? 'checked' : '' }} checked/>
-                        <x-admin.input-label for="volume-program" class="ml-2">
-                            Belum terlaksana
-                        </x-admin.input-label>
-                    </div>
-                    <div class="flex items-center flex-1 justify-center">
-                        <input type="radio" name="information" value="terlaksana"
-                            class="h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-slate-600 checked:border-slate-800"
-                            {{ (old('information', isset($selectedProgram) ? $selectedProgram->information : '') == 'terlaksana') ? 'checked' : '' }} />
-                            <x-admin.input-label for="volume-program" class="ml-2">
-                                Terlaksana
-                            </x-admin.input-label>
-                    </div>
-                    <div class="flex items-center flex-1 justify-center">
-                        <input type="radio" name="information" value="tidak_terlaksana"
-                            class="h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-slate-600 checked:border-slate-800"
-                            {{ (old('information', isset($selectedProgram) ? $selectedProgram->information : '') == 'tidak_terlaksana') ? 'checked' : '' }} />
-                            <x-admin.input-label for="volume-program" class="ml-2">
-                                Tidak terlaksana
-                            </x-admin.input-label>
-                    </div>
-                </nav>
+            <div x-data="{ selected: '{{ isset($selectedProgram) ? $selectedProgram->information : old('information', 'belum_terlaksana') }}' }"
+                class="w-full max-w-2xl flex flex-col rounded-lg bg-white dark:bg-zinc-900 shadow p-3">
+                <div class="flex flex-row justify-between">
+                    <label class="flex items-center">
+                        <input type="checkbox" name="information" value="belum_terlaksana"
+                            class="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring focus:ring-blue-300"
+                            :checked="selected === 'belum_terlaksana'"
+                            @change="selected = 'belum_terlaksana'; resetCheckboxes('belum_terlaksana')">
+                        <span class="text-gray-700 text-sm ml-2">Belum terlaksana</span>
+                    </label>
+
+                    <label class="flex items-center">
+                        <input type="checkbox" name="information" value="terlaksana"
+                            class="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring focus:ring-blue-300"
+                            :checked="selected === 'terlaksana'"
+                            @change="selected = 'terlaksana'; resetCheckboxes('terlaksana')">
+                        <span class="text-gray-700 text-sm ml-2">Terlaksana</span>
+                    </label>
+
+
+                    <label class="flex items-center">
+                        <input type="checkbox" name="information" value="tidak_terlaksana"
+                            class="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring focus:ring-blue-300"
+                            :checked="selected === 'tidak_terlaksana'"
+                            @change="selected = 'tidak_terlaksana'; resetCheckboxes('tidak_terlaksana')">
+                        <span class="text-gray-700 text-sm ml-2">Tidak terlaksana</span>
+                    </label>
+                </div>
             </div>
-            <span class="text-gray-400 text-xs mt-2 block">{{ __('* Secara default, keterangan akan terpilih sebagai Belum terlaksana') }}</span>
+            <span
+                class="text-gray-400 text-xs mt-2 block">{{ __('* Secara default, keterangan akan terpilih sebagai Belum terlaksana') }}</span>
         </div>
     </div>
-    @endsection
-
-
+@endsection
