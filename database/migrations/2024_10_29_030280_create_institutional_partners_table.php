0b12dd0a3cda2d73cab472429efabaa66b7b9f82
Gilang <19211020@bsi.ac.id>
2024-12-13 21:45:32 +0700

    refactor(migration): move example instituional partners initiate to Seeder

--- database/migrations/2024_10_29_030280_create_institutional_partners_table.php
+++ database/migrations/2024_10_29_030280_create_institutional_partners_table.php
@@ -17,46 +17,7 @@ return new class extends Migration
             $table->string('name', length: 255);
             $table->timestamps();
         });
-
-        // Insert default records
-        DB::table('institutional_partners')->insert([
-        [
-            'id' => 'MITRA-001',
-            'name' => 'MITRA PERTAMA',
-            'created_at' => now(),
-            'updated_at' => now()
-        ],
-        [
-            'id' => 'MITRA-002',
-            'name' => 'MITRA KE DUA',
-            'created_at' => now(),
-            'updated_at' => now()
-        ],
-        [
-            'id' => 'MITRA-003',
-            'name' => 'MITRA KE TIGA',
-            'created_at' => now(),
-            'updated_at' => now()
-        ],
-        [
-            'id' => 'MITRA-004',
-            'name' => 'MITRA KE EMPAT',
-            'created_at' => now(),
-            'updated_at' => now()
-        ],
-        [
-            'id' => 'MITRA-005',
-            'name' => 'MITRA KE LIMA',
-            'created_at' => now(),
-            'updated_at' => now()
-        ],
-        [
-            'id' => 'MITRA-006',
-            'name' => 'MITRA KE ENAM',
-            'created_at' => now(),
-            'updated_at' => now()
-        ],
-    ]);
+        
     }
 
     /**
