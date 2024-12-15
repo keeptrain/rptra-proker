8a9f6a6bbbbf3a011405d95c788a8274dddfa1c7
Gilang <19211020@bsi.ac.id>
2024-12-15 18:12:15 +0700

    refactor(migration): move create schema pivot partners_transaction

--- database/migrations/2024_10_29_030347_create_transaction_programs_table.php
+++ database/migrations/2024_10_29_030347_create_transaction_programs_table.php
@@ -29,19 +29,6 @@ return new class extends Migration {
             // Foreign key to priority_program (id)
             $table->foreign('principal_program_id')->references('id')->on('principal_programs')->onUpdate('cascade')->onDelete('cascade');
         });
-
-        // Pivot
-        Schema::create('institutional_partner_transaction_program', function (Blueprint $table) {
-            $table->increments('id')->primary();
-            $table->unsignedInteger('transaction_program_id')->nullable(true);
-            $table->string('institutional_partner_id', 50)->nullable(true);
-            //$table->timestamps();
-
-            $table->foreign('transaction_program_id', 'fk_transaction_program')->references('id')->on('transaction_programs')->onDelete('cascade');
-
-            // Menentukan nama constraint secara manual
-            $table->foreign('institutional_partner_id', 'fk_institutional_partner')->references('id')->on('institutional_partners')->onDelete('cascade');
-        });
     }
 
     /**
