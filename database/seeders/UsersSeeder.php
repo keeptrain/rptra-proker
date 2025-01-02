4a734ba7ef4d08c07cb2adf3b28eb75748b1773e
Gilang <19211020@bsi.ac.id>
2025-01-02 21:20:57 +0700

    refactor(seeder): add new data seeder for Users and apply to DatabaseSeeder

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'id' => 1,
+                'name' => 'Admin',
+                'email'=> 'admin',
+                'password'=> bcrypt('123'),
+
+            ],
+        ]);
+    }
+}
