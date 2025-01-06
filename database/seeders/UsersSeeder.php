fb36732cb992e648566ad583fe534ec6427b47e6
Gilang <19211020@bsi.ac.id>
2025-01-06 16:26:18 +0700

    refactor(seeder): add default profile image for users

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -18,7 +18,7 @@ class UsersSeeder extends Seeder
                 'name' => 'Admin',
                 'email'=> 'admin',
                 'password'=> bcrypt('123'),
-
+                'image' => 'images/profile-default.svg',
             ],
         ]);
     }
